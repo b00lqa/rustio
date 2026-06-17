affea8911750d0a774728afd49a370ede2b05b95
b00lqa <ruthwaelry@gmail.com>
2026-06-17 16:30:38 +0300

    separate request from main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
+    let resp = test_api("https://httpbin.org/ip").await?;
     println!("{resp:#?}");
     Ok(())
+}
+
+async fn test_api(url: &str) -> Result<HashMap<String, String>, Box<dyn Error>> {
+    Ok(reqwest::get(url)
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?)
 }
